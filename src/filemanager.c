9d0d317ea8972dcbf269a72fcbce509e374522bd
Bradley Eck <7848456+bradleyjeck@users.noreply.github.com>
2020-10-26 16:18:33 +0000

    return value of fgets

--- src/filemanager.c
+++ src/filemanager.c
@@ -133,7 +133,8 @@ int printf_file(file_handle_t *file_handle, const char *format, ... )
 
 int gets_file(char *str, int num, file_handle_t *file_handle)
 {
-    fgets(str, num, file_handle->file);
+    char* res;
+    res = fgets(str, num, file_handle->file);
     return 0;
 }
 
