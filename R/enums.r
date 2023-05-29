e9fdb1a58511aa882e306fdea616d40c59b718ee
Brad Eck <7848456+bradleyjeck@users.noreply.github.com>
2023-05-29 11:39:33 +0100

    enums

---
+++ R/enums.r
@@ -0,0 +1,12 @@
+
+
+linkTypes <- c(
+ "EN_CVPIPE",#      = 0,  //!< Pipe with check valve
+ "EN_PIPE",#      = 1,  //!< Pipe
+ "EN_PUMP",#      = 2,  //!< Pump
+ "EN_PRV",#      = 3,  //!< Pressure reducing valve
+ "EN_PSV",#      = 4,  //!< Pressure sustaining valve
+ "EN_PBV",#      = 5,  //!< Pressure breaker valve
+ "EN_FCV",#      = 6,  //!< Flow control valve
+ "EN_TCV",#      = 7,  //!< Throttle control valve
+ "EN_GPV")#      = 8   //!< General purpose valve
