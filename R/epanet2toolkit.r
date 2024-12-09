24f89f348a7f1d99f1be162acad4e7efac84cf88
Bradley Eck <7848456+bradleyjeck@users.noreply.github.com>
2024-12-09 17:33:06 +0000

    new src for pkg doc

--- R/epanet2toolkit.r
+++ R/epanet2toolkit.r
@@ -1,19 +1,2 @@
-#*************************************
-#
-#  (C) Copyright IBM Corp. 2017
-#
-#  Author:  Ernesto Arandia & Bradley J Eck
-#
-#*************************************
-
-#' epanet2toolkit 
-#'
-#' Package for using EPANET 2 from R.  Run a full EPANET simulation
-#' using \code{\link{ENepanet}} or build a custom simulation starting with toolkit
-#' functions like \code{\link{ENopen}}. 
-#'
-#' @docType package
-#' @aliases epanet2toolkit-package
-#' @name epanet2toolkit
-#' @author Ernesto Arandia & Bradley J. Eck
-NULL
+#' @keywords internal 
+"_PACKAGE"
